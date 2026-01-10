21e22cbf50f28ec19b2e9592dea57879335db7d0
Daniel Sinn <daniel.p.sinn@gmail.com>
2026-01-10 17:05:48 -0500

    Remove non-Warframe embed

--- kim-convo-locator.php
+++ kim-convo-locator.php
@@ -324,5 +324,4 @@
 	document.getElementById("query").oninput = doLocate;
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script src="https://censorcanary.org/censorcanary.js" defer></script>
 </html>

6b5c70bd09326bc1a02e6e4a4b10a7533365fd67
Daniel Sinn <daniel.p.sinn@gmail.com>
2026-02-07 22:40:30 -0500

    Upgrade to ESLint v9

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-deprecated': 'warn',
+    },
+  },
+];

983d861d27c4ba58c8ffc03c9a62def72e3bdee7
Daniel Sinn <daniel.p.sinn@gmail.com>
2026-04-04 00:54:58 -0400

    Fix favicon for incursions page

--- incursions.php
+++ incursions.php
@@ -4,7 +4,7 @@
 	<title>Steel Path Incursion Schedule | browse.wf</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-	<link rel="icon" href="https://browse.wf/Lotus/Interface/Icons/Player/SteelPathLogo.png">
+	<link rel="icon" href="https://browse.wf/Lotus/Interface/Icons/Categories/GrimoireModIcon.png">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" integrity="sha384-XGjxtQfXaH2tnPFa9x+ruJTuLE3Aa6LhHSWRr1XeTyhezb4abCG4ccI5AkVDxqC+" crossorigin="anonymous">
 	<link rel="stylesheet" href="src/incursions.css">
 </head>

25c29de50b185681319a5c23ebf6fefb2982dd0a
Daniel Sinn <daniel.p.sinn@gmail.com>
2026-04-09 18:57:02 -0400

    Dev server tweaks
    * Don't let PHP take down all other processes when it dies for whatever reason
    * Notify when the dev server process exits unexpectedly

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
+import process from 'node:process';
 import {concurrently} from 'concurrently';
 
-concurrently(
-	[
-		{command: 'npx tsc --watch', name: 'tsc'},
-		{command: 'node esbuild.config.js --watch', name: 'esbuild'},
-		{command: 'node scripts/render-pages.js --watch', name: 'render', restartTries: -1},
-		{command: 'vite --config vitest.config.ts', name: 'vite'},
-	],
-	{killOthers: ['failure']},
-);
+const {result} = concurrently([
+	{command: 'npx tsc --watch', name: 'tsc'},
+	{command: 'node esbuild.config.js --watch', name: 'esbuild'},
+	{command: 'node scripts/render-pages.js --watch', name: 'render', restartTries: -1},
+	{command: 'vite --config vitest.config.ts', name: 'vite'},
+]);
+
+try {
+	await result;
+} catch {
+	console.error('\u0007\n*** A dev server process exited unexpectedly. ***\n');
+	process.exit(1);
+}
